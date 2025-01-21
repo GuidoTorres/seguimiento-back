521df0bcde981e0c3b0358b7afa39fe58e19e5d5
Hector <gt12930@GMAIL.com>
2025-01-21 11:19:00 -0500

    feature: cotizacion sql

--- src/controllers/cotizaciones.js
+++ src/controllers/cotizaciones.js
@@ -16,7 +16,7 @@ const obtenerDatosAPI = async (tipo) => {
       SELECT        P.NRO_PEDIDO, P.MOTIVO_PEDIDO, CC.NOMBRE_DEPEND, P.TIPO_BIEN
       FROM            SIG_PEDIDOS AS P
                       INNER JOIN SIG_CENTRO_COSTO AS CC ON (CC.ANO_EJE = P.ANO_EJE AND CC.SEC_EJEC=P.SEC_EJEC AND CC.CENTRO_COSTO = P.CENTRO_COSTO)
-      WHERE        (P.ANO_EJE = '2025') AND (P.TIPO_BIEN =:tipo)      `,
+      WHERE        (P.ANO_EJE = '2025') AND (P.TIPO_BIEN =:tipo) AND (P.TIPO_PEDIDO =2)      `,
     {
       replacements: { tipo },
       type: QueryTypes.SELECT,
